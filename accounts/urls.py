7d524c60cb76b5f680ffdd9633c601733527301c
manthanchauhan913 <manthanchauhan913@gmail.com>
2019-12-05 15:11:06 +0530

    basic login-logout working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from accounts import views as views
 
 urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(template_name='registeration/login.html'), name='login'),
 ]
