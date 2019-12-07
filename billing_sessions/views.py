fe82190e2b9be5c9fbcab5c2f0582e8ae7abb5d7
manthanchauhan913 <manthanchauhan913@gmail.com>
2019-12-07 18:14:26 +0530

    pop up

--- billing_sessions/views.py
+++ billing_sessions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from billing_sessions.forms import SessionCreationForm
 from billing_sessions.models import BillingSession
@@ -22,6 +22,13 @@ class Home(LoginRequiredMixin, View):
                                               'form': form})
 
     def post(self, request):
-        print(request.POST)
+        form = SessionCreationForm(request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/sessions/home')
+        else:
+            return render(request, self.template, {'form': form})
+
 
 
